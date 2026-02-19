c9b96f65c4cb2b431571663e2152f14d06d05db6
Jieun Lee <jieunlee.johnny@gmail.com>
2026-02-19 17:01:22 +0900

    Update index.jsx

--- src/pages/mainPage/index.jsx
+++ src/pages/mainPage/index.jsx
@@ -1,4 +1,4 @@
-import Footer from '../../common/footer/footer';
+import Footer from '../../common/footer/Footer';
 import Header from '../../common/header/header';
 import Category from '../../components/mainPage/category/Category';
 import Community from '../../components/mainPage/community/Community';
